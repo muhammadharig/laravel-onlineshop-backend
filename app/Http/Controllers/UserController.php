8efbb9611f9d378f1061fb04f1eeebd8b37219b7
muhammadharig <mharigunawansyah@gmail.com>
2024-01-17 16:53:05 +0700

    membuat UserController beserta function nya

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // index
+    public function index()
+    {
+        return view('pages.dashboard');
+    }
+
+    // create
+    public function create()
+    {
+        return view('pages.dashboard');
+    }
+
+    // store
+    public function store(Request $request)
+    {
+        return view('pages.dashboard');
+    }
+
+    // show
+    public function show($id)
+    {
+        return view('pages.dashboard');
+    }
+
+    // edit
+    public function edit($id)
+    {
+        return view('pages.dashboard');
+    }
+
+    // update
+    public function update(Request $request, $id)
+    {
+        return view('pages.dashboard');
+    }
+
+    // destroy
+    public function destroy($id)
+    {
+        return view('pages.dashboard');
+    }
+}
