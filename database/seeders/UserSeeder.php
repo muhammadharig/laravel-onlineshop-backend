3ae90e77eb8ad04cf9cbf636530351d1b720b13a
muhammadharig <mharigunawansyah@gmail.com>
2024-01-17 14:54:22 +0700

    membuat UserSeeder.php dan membuat seed user random dan manual

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // membuat 9 random user
+        \App\Models\User::factory(9)->create();
+
+        // membuat manual user
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'hari@fic12.com',
+            'password' => Hash::make('hari8061'),
+        ]);
+
+    }
+}
